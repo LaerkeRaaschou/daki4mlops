602789f841e9408e494d8fdcc38d7c4499f36a15
anneinger <anne@ingerslev.info>
2026-02-05 15:25:30 +0100

    tilføjelser til trainscript

--- train.py
+++ train.py
@@ -1,10 +1,13 @@
 import torch
 import torch.nn as nn
-from torch import optim
+from torch.optim import SGD
 from torch.optim.lr_scheduler import StepLR
 import torchvision 
-from model import resnet18 as resnet
-from data import dataloader
+from .model.resnet18 import ResNet18
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
+from data.dataloader import *
 import os
 import wandb
 
@@ -21,33 +24,31 @@ def train_model(model, dataloader, optimizer, device, epoch):
         optimizer.zero_grad()
 
         y_pred = model(x)
-        loss = loss_function(y, y_pred)
+        loss = loss_function(y_pred, y)
         loss.backward()
         optimizer.step()
 
         if i % 200 == 0:
             print("Train Epoch: %s, Iteration: %s, Train Loss: %s" % (epoch, i, loss.item()))
             wandb.log({"Train Loss": loss.item()})
-    return loss.item
+    return loss.item()
 
-
+@torch.no_grad()
 def val_model(model, dataloader, device, epoch):
     model.eval()
-    loss = 0.0
+    loss_function = nn.CrossEntropyLoss()
 
-    for x, y in enumerate(dataloader):
+    for i, (x, y) in enumerate(dataloader):
         x = x.to(device)
         y = y.to(device)
         
         y_pred = model(x)
-        loss = loss_function(y, y_pred)
-        loss.backward()
-        optimizer.step()
+        loss = loss_function(y_pred, y)
 
         if i % 200 == 0:
             print("Val Epoch: %s, Iteration: %s, Val Loss: %s" % (epoch, i, loss.item()))
             wandb.log({"Val Loss": loss.item()})
-    return loss.item
+    return loss.item()
 
 
 
@@ -55,13 +56,13 @@ def main():
 
     # Set variables
     num_epochs = 10
-    batch_size = 10
+    batch_size = 64
     learning_rate = 1e-2
-    loss = 0
-    epoch = 1
+    
 
-    # Start wandb
-    wandb.login()
+    # Data path
+    data_path = "data/tiny-imagenet-200"
+
 
     # Use device
     if torch.cuda.is_available():
@@ -71,18 +72,26 @@ def main():
         device = torch.device("cpu")
         print("cpu")
 
-    # Download data
-    data_path = ""
 
-    # Get data loader
-    train_set = x(data_path, split="train-standard", train=True)
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=1, shuffle=False, num_workers=16)
+    # Start wandb
+    wandb.login()
+    wandb.init(project="tiny-imagenet-resnet18")
+
 
     # Initialize model 
-    model = resnet.to(device)
+    model = ResNet18(num_classes=200).to(device)
+
+
     # Define optimizer
-    optimizer = optim.SGD(model_parameters(), lr=learning_rate, momentum=0.9, weight_decay=5e-4)
-    schedular = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+    optimizer = SGD(model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=5e-4)
+    scheduler = StepLR(optimizer, step_size=3, gamma=0.1)
+
+    best = 0.0
+    for epoch in range(1, num_epochs + 1):
+        train_loss = train_model(model, train_loader, optimizer, device, epoch)
+        val_loss = val_model(model, test_loader, device, epoch)
+
+        scheduler.step()
 
 if __name__ == "__main__":
     main()
