69b7f60252088e7d8dfa192d0f323edcc2cf61b9
Laerkeraaschou <laerke@raaschou.net>
2026-02-05 11:27:00 +0100

    Improved dataloader, changes to test set

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,17 +1,15 @@
-from torch.utils.data import Dataset, DataLoader
-from torchvision import datasets, transforms
 import os
 from glob import glob
-from torchvision.io import decode_image
 from pathlib import Path
+import torch
+from torchvision.io import decode_image
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets, transforms
 
-#--
-#CONFIG
 batch_size = 64
-#--
 
 def map_train_id_to_class_id(dataset, idx):
-    """ Mapping the continues training id to dataset class id for specific index, idx """
+    """Map the continuous training id to dataset class id for specific index, idx"""
 
     idx_to_class = {v: k for k, v in dataset.class_to_idx.items()}
     img, label = dataset[idx]
@@ -20,73 +18,82 @@ def map_train_id_to_class_id(dataset, idx):
 
 
 def map_class_id_to_class_label(class_id):
-    """ Mapping dataset class_id to corresponding class label """
+    """Map dataset class_id to corresponding class label"""
 
     # Open dataset mapping txt file
-    with open("data/tiny-imagenet-200/words.txt", "r") as f:
-        for line in f:
+    with open("data/tiny-imagenet-200/words.txt", "r") as file:
+        class_label = None
+        for line in file:
             # Split each line into sections
-            parts = line.strip().split("\t")
+            fields = line.strip().split("\t")
             # Check if the class_id matches this lines mapping
-            if class_id == parts[0]:
-                class_label = parts[1]
+            if class_id == fields[0]:
+                class_label = fields[1]
                 break
-            else:
-                pass
     return class_label
 
 
-class Tiny_imagnet_testset(Dataset):
-    def __init__(self, path, transform):
+class TinyImagenetTestset(Dataset):
+    def __init__(self, data_dir, transform):
         # Find all paths to images inside the pathfolder
-        self.path = sorted(glob(os.path.join(path, "**", "*.JPEG"), recursive = True))
+        self.image_paths = sorted(glob(os.path.join(data_dir, "**", "*.JPEG"), recursive=True))
         self.transform = transform
-        self.samples = []
-
         self.mapping = {}
 
         # Create mapping for each test image to corresponding class_id
-        with open(f'{path + "/val_annotations.txt"}', "r") as f:
-            for line in f:
-                parts = line.strip().split("\t")
-                filename = parts[0]
-                class_id = parts[1]
+        with open(os.path.join(data_dir, "val_annotations.txt"), "r") as file:
+            for line in file:
+                fields = line.strip().split("\t")
+                filename = fields[0]
+                class_id = fields[1]
                 self.mapping[filename] = class_id
-
-        # Save decoded image and class_id
-        for image_path in self.path:
-            img = decode_image(image_path)
-            filename = Path(image_path).name
-            class_id = self.mapping[filename]
-
-            self.samples.append((img, class_id))
     
     def __len__(self):
         return len(self.samples)
 
     def __getitem__(self, index):
-        return self.samples[index]
+        # Decode path to image
+        image_path = self.image_paths[index]
+        img = decode_image(image_path)
+        # Map filename to class id
+        filename = Path(image_path).name
+        class_id = self.mapping[filename]
+
+        # Use transforms
+        if self.transform:
+            img = self.transform(img)
+        
+        sample = (img, class_id)
+        return sample
 
 
 def main():
     """ How to use the functions and methods in this module """
     # Define transforms for the training set
-    transform = transforms.Compose([transforms.Resize((64, 64)),
-                                     transforms.ToTensor()])
+    transform_train = transforms.Compose([transforms.Resize((64, 64)),
+                                          transforms.ToTensor(),
+                                          transforms.Normalize(mean=(0.485, 0.456, 0.406), 
+                                                               std=(0.229, 0.224, 0.225))]
+                                        )
+    
+    transform_test = transforms.Compose([transforms.Resize((64, 64)),
+                                    transforms.ConvertImageDtype(torch.float32),
+                                    transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))]
+                                    )
     
     # Training set made up of img and train_id
-    train_dataset = datasets.ImageFolder(root="data/tiny-imagenet-200/train", transform=transform)
-    train_loader = DataLoader(train_dataset, batch_size = batch_size, shuffle = True)
+    train_dataset = datasets.ImageFolder(root="data/tiny-imagenet-200/train", transform=transform_train)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
     # How to use function map_train_id_to_class_id
-    print(map_train_id_to_class_id(train_dataset, train_dataset.__getitem__(0)[1]))
+    print(map_train_id_to_class_id(train_dataset, train_dataset[0][1]))
 
     # Test set made up of img and class_id
-    test_dataset = Tiny_imagnet_testset("data/tiny-imagenet-200/val", transform=transform)
-    test_loader = DataLoader(test_dataset, batch_size = 1, shuffle = False)
+    test_dataset = TinyImagenetTestset("data/tiny-imagenet-200/val", transform=transform_test)
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
     # How to use map_class_id_to_class_label
-    print(map_class_id_to_class_label(test_dataset.__getitem__(0)[1]))
+    print(map_class_id_to_class_label(test_dataset[0][1]))
 
 if __name__ == "__main__":
     main()
