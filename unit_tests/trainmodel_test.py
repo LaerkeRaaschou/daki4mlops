da0fb1c5ffee87ba61267e27ad9848985bec1ee5
anneinger <anne@ingerslev.info>
2026-03-13 13:52:33 +0100

    fixed unittest trainmodel

--- unit_tests/trainmodel_test.py
+++ unit_tests/trainmodel_test.py
@@ -21,15 +21,19 @@ def _setup_case():
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
 
-    return model, loader, criterion, optimizer, device
+    epoch = 1
+
+    local_rank = 0
+
+    return model, loader, criterion, optimizer, device, epoch, local_rank
 
 
 def test_train_model_returns_finite_loss(monkeypatch):
     monkeypatch.setattr(train.wandb, "log", lambda payload: None)
 
-    model, loader, criterion, optimizer, device = _setup_case()
+    model, loader, criterion, optimizer, epoch, device, local_rank = _setup_case()
 
-    avg_loss = train.train_model(model, criterion, loader, optimizer, device, epoch=1)
+    avg_loss = train.train_model(model, criterion, loader, optimizer, epoch, device, local_rank)
 
     assert isinstance(avg_loss, float)
     assert torch.isfinite(torch.tensor(avg_loss))
@@ -38,11 +42,11 @@ def test_train_model_returns_finite_loss(monkeypatch):
 def test_train_model_updates_weights(monkeypatch):
     monkeypatch.setattr(train.wandb, "log", lambda payload: None)
 
-    model, loader, criterion, optimizer, device = _setup_case()
+    model, loader, criterion, optimizer, epoch, device, local_rank = _setup_case()
 
     w_before = model[1].weight.detach().clone()
 
-    _ = train.train_model(model, criterion, loader, optimizer, device, epoch=1)
+    _ = train.train_model(model, criterion, loader, optimizer, epoch, device, local_rank)
 
     w_after = model[1].weight.detach()
     assert not torch.allclose(w_before, w_after), "Expected weights to change after training"
@@ -52,8 +56,10 @@ def test_train_model_logs_something(monkeypatch):
     logged = []
     monkeypatch.setattr(train.wandb, "log", lambda payload: logged.append(payload))
 
-    model, loader, criterion, optimizer, device = _setup_case()
+    model, loader, criterion, optimizer, epoch, device, local_rank = _setup_case()
 
-    _ = train.train_model(model, criterion, loader, optimizer, device, epoch=1)
+    _ = train.train_model(model, criterion, loader, optimizer, epoch, device, local_rank)
 
     assert len(logged) >= 1, "Expected at least one wandb.log call"
+
+    
