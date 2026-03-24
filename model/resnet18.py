6618cb8b0a617eb99f4636381ae5e06ae4c8ad02
Laerkeraaschou <laerke@raaschou.net>
2026-03-24 10:25:52 +0100

    Finished quantization

--- model/resnet18.py
+++ model/resnet18.py
@@ -1,17 +1,27 @@
 import torch
 import torch.nn as nn
+import torch.quantization
 
 
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1, downsample=None):
         super(ResidualBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=3,
+            stride=stride,
+            padding=1,
+            bias=False,
+        )
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(
+            out_channels, out_channels, kernel_size=3, padding=1, bias=False
+        )
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.downsample = downsample
-
+        self.skip_add = nn.quantized.FloatFunctional()
 
     def forward(self, x):
         identity = x
@@ -30,31 +40,32 @@ class ResidualBlock(nn.Module):
             identity = self.downsample(x)
 
         # Add skip connection
-        out += identity
+        out = self.skip_add.add(out, identity)
         out = self.relu(out)
-        
-        return out 
-    
+
+        return out
+
 
 def initialize_weights(m):
     if isinstance(m, nn.Conv2d):
-        nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+        nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
     elif isinstance(m, nn.BatchNorm2d):
         nn.init.constant_(m.weight, 1)
         nn.init.constant_(m.bias, 0)
 
-        
 
 class ResNet18(nn.Module):
     def __init__(self, num_classes=1000):
         super(ResNet18, self).__init__()
+        self.quant = torch.quantization.QuantStub()
+        self.dequant = torch.quantization.DeQuantStub()
         self.in_channels = 64
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        #Define the layers based on ResNet-18 architecture
+        # Define the layers based on ResNet-18 architecture
         self.layer1 = self._make_layer(64, 2)
         self.layer2 = self._make_layer(128, 2, stride=2)
         self.layer3 = self._make_layer(256, 2, stride=2)
@@ -64,22 +75,29 @@ class ResNet18(nn.Module):
 
         # Initialize weights
         self.apply(initialize_weights)
-    
+
     def _make_layer(self, out_channels, blocks, stride=1):
         downsample = None
         if stride != 1 or self.in_channels != out_channels:
             downsample = nn.Sequential(
-                nn.Conv2d(self.in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels)
+                nn.Conv2d(
+                    self.in_channels,
+                    out_channels,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False,
+                ),
+                nn.BatchNorm2d(out_channels),
             )
         layers = [ResidualBlock(self.in_channels, out_channels, stride, downsample)]
         self.in_channels = out_channels
         for _ in range(1, blocks):
             layers.append(ResidualBlock(out_channels, out_channels))
         return nn.Sequential(*layers)
-    
 
     def forward(self, x):
+        x = self.quant(x)
+
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
@@ -93,11 +111,12 @@ class ResNet18(nn.Module):
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
+        x = self.dequant(x)
 
         return x
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     tensor = torch.rand([1, 3, 64, 64])
     model = ResNet18()
 
@@ -105,10 +124,10 @@ if __name__ == '__main__':
     total_params = sum(p.numel() for p in model.parameters())
     print(f"{total_params:,} total parameters.")
     total_trainable_params = sum(
-        p.numel() for p in model.parameters() if p.requires_grad)
+        p.numel() for p in model.parameters() if p.requires_grad
+    )
     print(f"{total_trainable_params:,} training parameters.")
-    
-    print("64 size image")
-    
-    output = model(tensor)
 
+    print("64 size image")
+
+    output = model(tensor)
---
+++ quantization.py
@@ -0,0 +1,80 @@
+import torch
+import torch.quantization
+import torch.nn
+import os
+import datetime
+from model.resnet18 import ResNet18
+
+torch.backends.quantized.engine = "qnnpack"
+
+
+def quantize_model(full_model, quantized_model):
+    torch.backends.quantized.engine = "qnnpack"
+    model = ResNet18(num_classes=200)
+    model = torch.compile(model, backend="eager")
+    model.load_state_dict(torch.load(full_model, map_location=torch.device("cpu")))
+    model = model._orig_mod
+    model.eval()
+    model.qconfig = torch.quantization.get_default_qconfig("qnnpack")
+    model_prepared = torch.quantization.prepare(model, inplace=False)
+    with torch.no_grad():
+        for _ in range(10):
+            input_tensor = torch.randn(1, 3, 64, 64, dtype=torch.float)
+            model_prepared(input_tensor)
+    model_quantized = torch.quantization.convert(model_prepared, inplace=False)
+    torch.save(model_quantized.state_dict(), quantized_model)
+    print("Quantized model success")
+
+
+def load_quantized_model(quantized_model_path):
+    torch.backends.quantized.engine = "qnnpack"
+    model = ResNet18(num_classes=200)
+    model.eval()
+    model.qconfig = torch.quantization.get_default_qconfig("qnnpack")
+    torch.quantization.prepare(model, inplace=True)
+    torch.quantization.convert(model, inplace=True)
+    model.load_state_dict(
+        torch.load(quantized_model_path, weights_only=False, map_location="cpu")
+    )
+    return model
+
+
+def run_inference(model_path, data, q):
+    if q:
+        model = load_quantized_model(model_path)
+    else:
+        model = ResNet18(num_classes=200)
+        model = torch.compile(model, backend="eager")
+        model.load_state_dict(
+            torch.load(model_path, weights_only=False, map_location="cpu")
+        )
+        model = model._orig_mod
+    model.eval()
+    with torch.no_grad():
+        _ = model(data)  # warmup
+    start = datetime.datetime.now()
+    with torch.no_grad():
+        _ = model(data)
+    end = datetime.datetime.now()
+    return (end - start).microseconds
+
+
+def main():
+    full_model = "model/trained_models/resnet_18_classifier_best_acc_epoch15.pt"
+    quantized_model = "model/quantized_models/quantized.pt"
+    device = "cpu"
+    data = torch.randn(1, 3, 64, 64, dtype=torch.float).to(device)
+    if os.path.exists(quantized_model):
+        f_time = run_inference(full_model, data, q=False)
+        q_time = run_inference(quantized_model, data, q=True)
+        print("Difference between models in microseconds")
+        print("float32:", f_time, "  qint8:", q_time)
+    else:
+        quantize_model(full_model, quantized_model)
+        f_time = run_inference(full_model, data, q=False)
+        q_time = run_inference(quantized_model, data, q=True)
+        print("float32:", f_time, "  qint8:", q_time)
+
+
+if __name__ == "__main__":
+    main()
